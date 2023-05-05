4ac7dd27281eb3a7b4a5a073a4746bcf1edb1fb3
seung-hun2 <jungsh2328@naver.com>
2023-05-05 23:06:58 +0900

    feat: 작가가 자신의 웹툰 회차 조회 Mock API 생성

--- webtoon-service/src/main/java/com/blockpage/webtoonservice/adaptor/web/EpisodeController.java
+++ webtoon-service/src/main/java/com/blockpage/webtoonservice/adaptor/web/EpisodeController.java
@@ -3,6 +3,7 @@ package com.blockpage.webtoonservice.adaptor.web;
 import com.blockpage.webtoonservice.adaptor.infrastructure.value.GenreType;
 import com.blockpage.webtoonservice.adaptor.infrastructure.value.PublicationDays;
 import com.blockpage.webtoonservice.adaptor.web.view.ApiResponseView;
+import com.blockpage.webtoonservice.adaptor.web.view.CreatorEpisodeView;
 import com.blockpage.webtoonservice.adaptor.web.view.EpisodeView;
 import com.blockpage.webtoonservice.adaptor.web.view.WebtoonDescribeView;
 import java.util.ArrayList;
@@ -19,19 +20,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class EpisodeController {
 
     @GetMapping("")
-    public ResponseEntity<ApiResponseView> episodeSearch(@RequestParam int webtoonId){
+    public ResponseEntity<ApiResponseView> episodeSearch(@RequestParam Long webtoonId) {
         List<EpisodeView> episodeView = new ArrayList<>();
         WebtoonDescribeView webtoonDescribeViews = null;
 
-        episodeView.add(new EpisodeView("1화","http://","230507",2200));
-        episodeView.add(new EpisodeView("2화","http://","230514",100));
-        episodeView.add(new EpisodeView("3화","http://","230521",400));
-        episodeView.add(new EpisodeView("4화","http://","230528",800));
+        episodeView.add(new EpisodeView("1화", "http://", "230507", 2200));
+        episodeView.add(new EpisodeView("2화", "http://", "230514", 100));
+        episodeView.add(new EpisodeView("3화", "http://", "230521", 400));
+        episodeView.add(new EpisodeView("4화", "http://", "230528", 800));
 
-        webtoonDescribeViews = new WebtoonDescribeView("침대다이부", "백고은",null,
-            PublicationDays.MONDAY, GenreType.COMIC,"https://user-images.githubusercontent.com/97498405/235885340-d63630ec-85ec-4801-bf73-ac83f96c3bd2.jpg",
+        webtoonDescribeViews = new WebtoonDescribeView("침대다이부", "백고은", null,
+            PublicationDays.MONDAY, GenreType.COMIC,
+            "https://user-images.githubusercontent.com/97498405/235885340-d63630ec-85ec-4801-bf73-ac83f96c3bd2.jpg",
             episodeView);
         return ResponseEntity.status(HttpStatus.OK).body(new ApiResponseView<>(webtoonDescribeViews));
+    }
+
+    @GetMapping("/creator")
+    public ResponseEntity<ApiResponseView> myWebtoonSearch(@RequestParam Long memberId, @RequestParam Long webtoonId) {
+        List<CreatorEpisodeView> creatorEpisodeViews = new ArrayList<>();
+
+        creatorEpisodeViews.add(new CreatorEpisodeView("1화다", 1, "https://user-images.githubusercontent.com/97498405/235885340-d63630ec-85ec-4801-bf73-ac83f96c3bd2.jpg", 2200,1000,"2023-05-01"));
+        creatorEpisodeViews.add(new CreatorEpisodeView("2화다", 2, "https://user-images.githubusercontent.com/97498405/235885340-d63630ec-85ec-4801-bf73-ac83f96c3bd2.jpg", 100, 20,"2023-05-08"));
+        creatorEpisodeViews.add(new CreatorEpisodeView("3화다", 3, "https://user-images.githubusercontent.com/97498405/235885340-d63630ec-85ec-4801-bf73-ac83f96c3bd2.jpg", 100, 20,"2023-05-15"));
+        creatorEpisodeViews.add(new CreatorEpisodeView("4화다", 4, "https://user-images.githubusercontent.com/97498405/235885340-d63630ec-85ec-4801-bf73-ac83f96c3bd2.jpg", 100, 20,"2023-05-22"));
+
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponseView<>(creatorEpisodeViews));
     }
 
 
