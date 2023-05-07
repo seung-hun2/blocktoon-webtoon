2b6eff8fa9df73471d6d3d5fb5f630b50f7212bf
seung-hun2 <jungsh2328@naver.com>
2023-05-07 15:00:03 +0900

    refactor: view 모델 builder 제거

--- webtoon-service/src/main/java/com/blockpage/webtoonservice/adaptor/web/view/WebtoonView.java
+++ webtoon-service/src/main/java/com/blockpage/webtoonservice/adaptor/web/view/WebtoonView.java
@@ -1,11 +1,9 @@
 package com.blockpage.webtoonservice.adaptor.web.view;
 
 import com.blockpage.webtoonservice.adaptor.infrastructure.entity.WebtoonEntity;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class WebtoonView {
 
     private String webtoonTitle;
@@ -15,14 +13,22 @@ public class WebtoonView {
     private int views;
     private int interestCount;
 
+    public WebtoonView(String webtoonTitle, String webtoonThumbnail, String creator, String illustrator, int views, int interestCount) {
+        this.webtoonTitle = webtoonTitle;
+        this.webtoonThumbnail = webtoonThumbnail;
+        this.creator = creator;
+        this.illustrator = illustrator;
+        this.views = views;
+        this.interestCount = interestCount;
+    }
+
     public static WebtoonView toViewFromEntity(WebtoonEntity webtoon) {
-        return WebtoonView.builder()
-            .creator(webtoon.getCreator())
-            .views(webtoon.getViews())
-            .interestCount(webtoon.getInterestCount())
-            .webtoonThumbnail(webtoon.getWebtoonThumbnail())
-            .webtoonTitle(webtoon.getWebtoonTitle())
-            .illustrator(webtoon.getIllustrator())
-            .build();
+        return new WebtoonView(
+            webtoon.getWebtoonTitle(),
+            webtoon.getWebtoonThumbnail(),
+            webtoon.getCreator(),
+            webtoon.getIllustrator(),
+            webtoon.getViews(),
+            webtoon.getInterestCount());
     }
 }
