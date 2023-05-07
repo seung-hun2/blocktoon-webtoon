c7e6ba2649d105e58d14b4aff7ed0c5501a73156
seung-hun2 <jungsh2328@naver.com>
2023-05-07 23:16:41 +0900

    feat: 웹툰 도메인 Post,Patch,delete mock API 생성

--- webtoon-service/src/main/java/com/blockpage/webtoonservice/adaptor/web/WebtoonController.java
+++ webtoon-service/src/main/java/com/blockpage/webtoonservice/adaptor/web/WebtoonController.java
@@ -6,6 +6,7 @@ import com.blockpage.webtoonservice.adaptor.infrastructure.value.GenreType;
 import com.blockpage.webtoonservice.adaptor.infrastructure.value.PublicationDays;
 import com.blockpage.webtoonservice.adaptor.infrastructure.value.WebtoonStatus;
 import com.blockpage.webtoonservice.adaptor.web.view.WebtoonView;
+import com.blockpage.webtoonservice.application.port.in.RequestWebtoon;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,51 @@ import java.util.stream.Collectors;
 public class WebtoonController {
 
     private final WebtoonRepository webtoonRepository;
+
+    @PostMapping()
+    public ResponseEntity enroll(@RequestBody RequestWebtoon requestWebtoon) {
+        // 웹툰 등록하는 서비스 로직 구현
+        return ResponseEntity.status(HttpStatus.CREATED).body("웹툰이 생성되었습니다.");
+    }
+
+    @PostMapping("/modifying")
+    public ResponseEntity modifyWaiting(@RequestBody RequestWebtoon requestWebtoon) {
+        // 웹툰 수정요청 서비스 로직 구현
+        return ResponseEntity.status(HttpStatus.CREATED).body("웹툰 수정 요청되었습니다.");
+    }
+
+    @PatchMapping("/admin/modifying")
+    public ResponseEntity receiveModifying(@RequestBody RequestWebtoon requestWebtoon, @RequestParam String result) {
+        // result 값에 따라 승인인지 반려인지 확인해서 넘겨줘야함
+        switch (result) {
+            case "accept":
+                return ResponseEntity.status(HttpStatus.OK).body("웹툰 수정요청이 승인되었습니다.");
+            case "reject":
+                return ResponseEntity.status(HttpStatus.OK).body("웹툰 수정요청이 반려되었습니다.");
+            default:
+                return (ResponseEntity) ResponseEntity.status(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/remove")
+    public ResponseEntity removeWaiting(@RequestBody RequestWebtoon requestWebtoon) {
+        // 웹툰 수정요청 서비스 로직 구현
+        return ResponseEntity.status(HttpStatus.CREATED).body("웹툰 삭제요청되었습니다.");
+    }
+
+    @PatchMapping("/admin/remove")
+    public ResponseEntity receiveRemoving(@RequestBody RequestWebtoon requestWebtoon, @RequestParam String result) {
+        // result 값에 따라 승인인지 반려인지 확인해서 넘겨줘야함
+        switch (result) {
+            case "accept":
+                return ResponseEntity.status(HttpStatus.OK).body("웹툰 삭제요청이 승인되었습니다.");
+            case "reject":
+                return ResponseEntity.status(HttpStatus.OK).body("웹툰 삭제요청이 반려되었습니다.");
+            default:
+                return (ResponseEntity) ResponseEntity.status(HttpStatus.BAD_REQUEST);
+        }
+    }
+
 
     /**
      * @param weekdays 요일별 웹툰 조회
