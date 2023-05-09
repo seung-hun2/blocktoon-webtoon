4a72a37e36d6f89ee12f0f25e3c902c099a0a8d6
seung-hun2 <jungsh2328@naver.com>
2023-05-09 16:22:06 +0900

    refactor: 피드백 반영

--- webtoon-service/src/main/java/com/blockpage/webtoonservice/application/port/out/DomainPortImpl.java
+++ webtoon-service/src/main/java/com/blockpage/webtoonservice/application/port/out/DomainPortImpl.java
@@ -12,7 +12,6 @@ import com.google.cloud.storage.Storage;
 import java.io.IOException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,35 +21,43 @@ import org.springframework.web.multipart.MultipartFile;
 public class DomainPortImpl {
 
     private final WebtoonRepository webtoonRepository;
+    private final Storage storage;
     private String bucketName = "blockpage-bucket";
 
-    @Autowired
-    Storage storage;
+    public WebtoonEntity toEntity(RequestWebtoon webtoon, String webtoonMainImage, String webtoonThumbnail) {
+        return WebtoonEntity.builder()
+            .creator(webtoon.getCreator())
+            .webtoonStatus(WebtoonStatus.PUBLISH)
+            .webtoonDescription(webtoon.getWebtoonDescription())
+            .webtoonTitle(webtoon.getWebtoonTitle())
+            .webtoonMainImage(webtoonMainImage)
+            .webtoonThumbnail(webtoonThumbnail)
+            .creatorId(webtoon.getCreatorId())
+            .genreType(GenreType.findGenreTypeByKey(webtoon.getGenre()))
+            .publicationDays(PublicationDays.findPublicationDaysByKey(webtoon.getPublicationDays()))
+            .interestCount(0)
+            .illustrator(webtoon.getIllustrator())
+            .views(0)
+            .build();
+    }
 
     public Long webtoonEnroll(@RequestBody RequestWebtoon webtoon, MultipartFile main, MultipartFile thumbnail) throws IOException {
 
         String mainUUID = UUID.randomUUID().toString();
         BlobId blobId1 = BlobId.of(bucketName, mainUUID);
-        BlobInfo blobInfo1 = BlobInfo.newBuilder(blobId1).setContentType(main.getContentType()).build();
+        BlobInfo blobInfo1 = BlobInfo.newBuilder(blobId1)
+            .setContentType(main.getContentType())
+            .build();
         storage.create(blobInfo1, main.getBytes());
 
         String thumbnailUUID = UUID.randomUUID().toString();
         BlobId blobId2 = BlobId.of(bucketName, thumbnailUUID);
-        BlobInfo blobInfo2 = BlobInfo.newBuilder(blobId2).setContentType(thumbnail.getContentType()).build();
+        BlobInfo blobInfo2 = BlobInfo.newBuilder(blobId2)
+            .setContentType(thumbnail.getContentType())
+            .build();
         storage.create(blobInfo2, thumbnail.getBytes());
 
-        WebtoonEntity webtoonEntity = WebtoonEntity.builder()
-            .creator(webtoon.getCreator())
-            .webtoonStatus(WebtoonStatus.PUBLISH)
-            .webtoonDescription(webtoon.getWebtoonDescription())
-            .webtoonTitle(webtoon.getWebtoonTitle())
-            .webtoonMainImage(mainUUID)
-            .webtoonThumbnail(thumbnailUUID)
-            .creatorId(webtoon.getCreatorId())
-            .genreType(GenreType.findGenreTypeByKey(webtoon.getGenre()))
-            .publicationDays(PublicationDays.findPublicationDaysByKey(webtoon.getPublicationDays()))
-            .interestCount(0)
-            .build();
+        WebtoonEntity webtoonEntity = toEntity(webtoon, mainUUID, thumbnailUUID);
 
         WebtoonEntity webtoons = webtoonRepository.save(webtoonEntity);
         return webtoons.getId();
