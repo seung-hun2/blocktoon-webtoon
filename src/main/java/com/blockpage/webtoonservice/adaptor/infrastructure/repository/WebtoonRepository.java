dd52427ee57343c790224c3b9b58bf0524c276eb
seung-hun2 <jungsh2328@naver.com>
2023-05-26 01:02:28 +0900

    feat: 웹툰 요청 Pagination

--- src/main/java/com/blockpage/webtoonservice/adaptor/infrastructure/repository/WebtoonRepository.java
+++ src/main/java/com/blockpage/webtoonservice/adaptor/infrastructure/repository/WebtoonRepository.java
@@ -4,10 +4,10 @@ import com.blockpage.webtoonservice.adaptor.infrastructure.entity.WebtoonEntity;
 import com.blockpage.webtoonservice.adaptor.infrastructure.value.GenreType;
 import com.blockpage.webtoonservice.adaptor.infrastructure.value.PublicationDays;
 import com.blockpage.webtoonservice.adaptor.infrastructure.value.WebtoonStatus;
-import com.google.api.gax.paging.Page;
-import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -23,7 +23,7 @@ public interface WebtoonRepository extends JpaRepository<WebtoonEntity, Long>, J
 
     List<WebtoonEntity> findByCreatorId(Long creatorId);
 
-    List<WebtoonEntity> findByCreatorIdAndWebtoonStatus(Long creatorId, WebtoonStatus webtoonStatus);
+    Page<WebtoonEntity> findByCreatorIdAndWebtoonStatus(Long creatorId, WebtoonStatus webtoonStatus, Pageable pageable);
 
     Optional<WebtoonEntity> findByWebtoonTitleAndCreatorIdAndWebtoonStatus(String title, Long creatorId, WebtoonStatus status);
 
