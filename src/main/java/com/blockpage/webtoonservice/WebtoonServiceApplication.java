30c73df83906ee7e1670d749e3c7667cb6c7f74f
seung-hun2 <jungsh2328@naver.com>
2023-05-26 10:08:14 +0900

    feat: @EnableEurekaClient 추가

--- src/main/java/com/blockpage/webtoonservice/WebtoonServiceApplication.java
+++ src/main/java/com/blockpage/webtoonservice/WebtoonServiceApplication.java
@@ -2,9 +2,11 @@ package com.blockpage.webtoonservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@EnableEurekaClient
 @SpringBootApplication
 public class WebtoonServiceApplication {
 
