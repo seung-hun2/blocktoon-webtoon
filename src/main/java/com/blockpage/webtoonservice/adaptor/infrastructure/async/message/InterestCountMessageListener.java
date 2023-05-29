87248c476be600e2d1c76ba17968e3867b247e70
seung-hun2 <jungsh2328@naver.com>
2023-05-29 18:20:40 +0900

    feat: kafka message listener 생성

---
+++ src/main/java/com/blockpage/webtoonservice/adaptor/infrastructure/async/message/InterestCountMessageListener.java
@@ -0,0 +1,27 @@
+package com.blockpage.webtoonservice.adaptor.infrastructure.async.message;
+
+import com.blockpage.webtoonservice.application.port.in.InterestUseCase;
+import com.blockpage.webtoonservice.application.port.in.InterestUseCase.InterestQuery;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class InterestCountMessageListener {
+
+    private final InterestUseCase interestUseCase;
+
+    @KafkaListener(
+        topics = "${spring.kafka.topicName1}",
+        groupId = "${spring.kafka.groupName1}",
+        containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void listenWithHeaders(@Payload InterestCountMessage interestCountMessage, @Headers MessageHeaders messageHeaders) {
+        interestUseCase.updateInterest(InterestQuery.toRequestInterest(interestCountMessage));
+    }
+
+}
