b256616a3b76ac48cbf230179c419b5a9a5e6f8d
seung-hun2 <jungsh2328@naver.com>
2023-05-29 15:47:51 +0900

    feat: kafka view count listener 생성

---
+++ src/main/java/com/blockpage/webtoonservice/adaptor/infrastructure/async/message/ViewCountMessageListener.java
@@ -0,0 +1,26 @@
+package com.blockpage.webtoonservice.adaptor.infrastructure.async.message;
+
+import com.blockpage.webtoonservice.application.port.in.ViewCountUseCase;
+import com.blockpage.webtoonservice.application.port.in.ViewCountUseCase.viewCountQuery;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ViewCountMessageListener {
+
+    private final ViewCountUseCase viewCountUseCase;
+
+    @KafkaListener(
+        topics = "${spring.kafka.topicName}",
+        groupId = "${spring.kafka.groupName}",
+        containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void listenWithHeaders(@Payload ViewCountMessage viewCountMessage, @Headers MessageHeaders messageHeaders) {
+        viewCountUseCase.updateViewCount(viewCountQuery.toViewCount(viewCountMessage));
+    }
+}
