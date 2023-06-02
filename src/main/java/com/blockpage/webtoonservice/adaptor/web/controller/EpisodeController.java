c3465d7932a355416477a24be67d3e498e3481be
seung-hun2 <jungsh2328@naver.com>
2023-06-02 10:45:24 +0900

    fix: controller 파라미터 변경

--- src/main/java/com/blockpage/webtoonservice/adaptor/web/controller/EpisodeController.java
+++ src/main/java/com/blockpage/webtoonservice/adaptor/web/controller/EpisodeController.java
@@ -18,6 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,7 +45,8 @@ public class EpisodeController {
     }
 
     @GetMapping("/creator")
-    public ResponseEntity<ApiResponseView<List<CreatorEpisodeView>>> myWebtoonSearch(@RequestParam Long memberId,
+    public ResponseEntity<ApiResponseView<List<CreatorEpisodeView>>> myWebtoonSearch(
+        @RequestHeader Long memberId,
         @RequestParam Long webtoonId) {
 
         List<ResponseCreatorEpisode> responseCreatorEpisodeList = episodeUseCase.findCreatorEpisode(webtoonId);
