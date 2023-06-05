b5f69e9553e2fb2e634ae695a4b4a38d0676ace9
seung-hun2 <jungsh2328@naver.com>
2023-06-05 10:20:05 +0900

    feat: DB 이중화를 위한 config 파일 생성

---
+++ src/main/java/com/blockpage/webtoonservice/adaptor/infrastructure/RoutingDatasource.java
@@ -0,0 +1,20 @@
+package com.blockpage.webtoonservice.adaptor.infrastructure;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+public class RoutingDatasource extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
+            log.info("읽기 전용 DB로 연결되었습니다.");
+            return "slave";
+        } else {
+            log.info("쓰기 전용 DB로 연결되었습니다.");
+            return "master";
+        }
+    }
+}
