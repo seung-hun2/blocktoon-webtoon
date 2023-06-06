106a73b6fbb418f71988e1b1c681dd003705d2a6
seung-hun2 <jungsh2328@naver.com>
2023-06-07 01:21:20 +0900

    feat: 관리자 웹툰 상세 조회 생성

--- src/main/java/com/blockpage/webtoonservice/adaptor/web/controller/WebtoonController.java
+++ src/main/java/com/blockpage/webtoonservice/adaptor/web/controller/WebtoonController.java
@@ -12,6 +12,7 @@ import org.checkerframework.checker.units.qual.A;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -75,6 +76,16 @@ public class WebtoonController {
 
         return ResponseEntity.status(HttpStatus.OK)
             .body(new ApiResponseView<>(webtoonViewList));
+
+    }
+
+    @GetMapping("/detail")
+    public ResponseEntity<ApiResponseView<WebtoonView>> getWebtoon(@RequestParam Long webtoonId) {
+        ResponseWebtoon responseWebtoon = webtoonUseCase.findWebtoon(webtoonId);
+        WebtoonView webtoonView = WebtoonView.toViewFromResponse(responseWebtoon);
+
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(new ApiResponseView<>(webtoonView));
 
     }
 
